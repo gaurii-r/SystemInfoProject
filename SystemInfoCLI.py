cee831adab74f625c441fe13e0a7cfacd2787047
Gauri Rahane <gaurirahane4@gmail.com>
2025-07-11 12:30:57 +0530

    Initial commit of SystemInfoProject

---
+++ SystemInfoCLI.py
@@ -0,0 +1,17 @@
+#import module
+
+import subprocess
+import os
+import psutil
+
+Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
+new = []
+
+for item in Id:
+    new.append(str(item.split('\r')[:-1]))
+for i in new:
+    print(i[2:-2])
+
+print(f"RAM Memory in Bytes : {psutil.virtual_memory()}")
+
+os.system("pause")
